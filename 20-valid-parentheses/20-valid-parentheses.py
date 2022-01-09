6c518349c98c60725737262ffeeb3a0108928edd
Altamas Bari <altamasbari@gmail.com>
2022-01-09 22:33:12 +0530

    Time: 49 ms (16.37%), Space: 14.3 MB (36.20%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        ch_opp = {')':'(', '}':'{', ']':'['}
+        for ch in s:
+            if ch in ch_opp:
+                if not stack or stack[-1] != ch_opp[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+        return not stack
