e623a090108ab41ab192f272fef460d4b4186c88
Altamas Bari <altamasbari@gmail.com>
2022-01-11 19:10:36 +0530

    Time: 668 ms (16.74%), Space: 15.6 MB (81.46%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        temp = []
+        self.recursion(res,temp,n,k)
+        return res
+    def recursion(self,res,temp,n,k):
+        if len(temp) == k:
+            res.append(temp.copy())
+            return 
+        if n == 0:
+            return 
+        temp.append(n)
+        self.recursion(res,temp,n-1,k)
+        temp.pop()
+        self.recursion(res,temp,n-1,k)
