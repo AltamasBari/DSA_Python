be6c36353f4b130e7be83c4057d16ac679f37737
Altamas Bari <altamasbari@gmail.com>
2022-01-28 20:58:36 +0530

    Time: 231 ms (27.06%), Space: 16.9 MB (19.91%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dict = {}
-        for x in nums:
-            dict[x] = dict.get(x,0) + 1;
-        for x in dict:
-            if dict.get(x) == 1:
-                return x
+        xor = nums[0]
+        for i in range(1,len(nums)):
+            xor ^= nums[i]
+        return xor
