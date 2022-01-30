2e04ed42f8e24512608dd3058ea277a110550459
Altamas Bari <altamasbari@gmail.com>
2022-01-30 20:58:50 +0530

    Time: 6120 ms (5.03%), Space: 14.6 MB (99.86%) - LeetHub

---
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        m = len(matrix)
+        n = len(matrix[0])
+        mn = float('inf')
+        for i in range(n):
+            dp = [[-1 for y in range(n)] for x in range(m)]
+            mn = min(self.recursion(0,i,m,dp,matrix),mn)
+        return mn
+    def recursion(self,i,j,n,dp,matrix):
+        if j < 0 or j >= n:
+            return float('inf')
+        if i == n-1:
+            return matrix[i][j]
+        if dp[i][j] != -1: return dp[i][j]
+        ans = matrix[i][j] + min(self.recursion(i+1,j,n,dp,matrix),self.recursion(i+1,j+1,n,dp,matrix),self.recursion(i+1,j-1,n,dp,matrix)) 
+        dp[i][j] = ans
+        return dp[i][j]
