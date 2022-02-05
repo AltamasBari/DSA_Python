be17a20a320807c7ee1a083edfa717a34e383980
Altamas Bari <altamasbari@gmail.com>
2022-02-05 23:55:18 +0530

    Time: 914 ms (18.54%), Space: 19.1 MB (15.83%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -2,22 +2,23 @@ class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
         # ans1: answer for one-interval subarray
         ans1 = cur = float('-inf')
+        S = 0
         for x in nums:
             cur = x + max(cur, 0)
             ans1 = max(ans1, cur)
-
+            S += x
         # ans2: answer for two-interval subarray, interior in A[1:]
         ans2 = cur = float('inf')
         for i in range(1, len(nums)):
             cur = nums[i] + min(cur, 0)
             ans2 = min(ans2, cur)
-        ans2 = sum(nums) - ans2
+        ans2 = S - ans2
 
         # ans3: answer for two-interval subarray, interior in A[:-1]
         ans3 = cur = float('inf')
         for i in range(len(nums)-1):
             cur = nums[i] + min(cur, 0)
             ans3 = min(ans3, cur)
-        ans3 = sum(nums) - ans3
+        ans3 = S - ans3
         
         return max(ans1, ans2, ans3)
