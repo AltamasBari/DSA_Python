c2ea00be46ad2a9be602be086bab8a289bbeb2f8
Altamas Bari <altamasbari@gmail.com>
2022-02-06 22:53:04 +0530

    Time: 223 ms (20.68%), Space: 18.2 MB (32.01%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -3,8 +3,10 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from queue import PriorityQueue
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        '''
         mp = {}
         for elem in lists:
             while elem:
@@ -17,3 +19,17 @@ class Solution:
                 res.next = ListNode(x)
                 res = res.next
         return head.next
+        '''
+        i = 0# Solving the PriorityQueue issue
+        dummy = ListNode(None)
+        curr = dummy
+        q = PriorityQueue()
+        for node in lists:
+            i += 1 # Solving the PriorityQueue issue
+            if node: q.put((node.val,i,node))
+        while q.qsize()>0:
+            i += 1 # Solving the PriorityQueue issue
+            curr.next = q.get()[2]
+            curr=curr.next
+            if curr.next: q.put((curr.next.val,i, curr.next))
+        return dummy.next
