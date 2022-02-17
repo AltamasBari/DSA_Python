fabb0f6ebe9d9798ea2c29de09b76f05ce18836d
Altamas Bari <altamasbari@gmail.com>
2022-02-17 15:50:25 +0530

    Time: 80 ms (82.03%), Space: 14 MB (75.64%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        ds = []
+        def recursion(idx,target,ds):
+            
+            if idx < 0:
+                return         
+            if target == 0:
+                res.append(ds.copy())
+            dont_take = recursion(idx-1,target,ds)
+            if target >= candidates[idx]:
+                ds.append(candidates[idx])
+                take = recursion(idx,target-candidates[idx],ds)
+                ds.remove(candidates[idx])
+            
+        recursion(len(candidates)-1,target,ds)
+        res = list(set(tuple(sorted(sub)) for sub in res))
+        return res
