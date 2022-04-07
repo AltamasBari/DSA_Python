cb432cde4f6b473525ce9876827541b702ddae06
Altamas Bari <altamasbari@gmail.com>
2022-04-07 17:23:21 +0530

    Time: 254 ms (38.07%), Space: 17.7 MB (40.65%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -2,7 +2,9 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m = len(word1)
         n = len(word2)
-        dp = [[-1 for j in range(n+1)] for i in range(m+1)]
+        '''
+        # recursion and memoization
+        dp = [[-1 for j in range(n)] for i in range(m)]
         def recursion(i,j):
             if i < 0:
                 return j + 1
@@ -21,3 +23,18 @@ class Solution:
                 return dp[i][j]
             
         return recursion(m-1,n-1)
+        '''
+        #tabulation
+        dp = [[0 for j in range(n+1)] for i in range(m+1)]
+        for j in range(n+1):
+            dp[0][j] = j
+        for i in range(m+1):
+            dp[i][0] = i
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])
+        return dp[m][n]
