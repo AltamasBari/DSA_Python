0549e7ed508000f5da4794c5a3995bc4536b2c36
Altamas Bari <altamasbari@gmail.com>
2022-05-12 12:59:51 +0530

    Time: 1538 ms (31.34%), Space: 25 MB (87.08%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
+        ''' 
+        Kadanes
+        
         max_so_far, cur = 0, 0
         
         for i in range(1,n):
             cur = max(cur + prices[i] - prices[i-1], 0)
             max_so_far = max(cur, max_so_far)
+            print(cur,max_so_far)
         return max_so_far   
+        
+        '''
+        # DP : remembering the past
+        min_so_far = prices[0]
+        max_profit = 0
+        for i in range(1,n):
+            curr_profit = prices[i] - min_so_far
+            max_profit = max(curr_profit,max_profit)
+            min_so_far = min(prices[i],min_so_far)
+        return max_profit
