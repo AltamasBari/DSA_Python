943c09889e9d01c1c8c84576e240172926c28f50
Altamas Bari <altamasbari@gmail.com>
2022-05-12 20:46:06 +0530

    Time: 134 ms (7.04%), Space: 45 MB (5.03%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        n = len(prices)
+        dp = [[-1 for j in range(2)] for i in range(n)]  
+        def recursion(idx,buy):
+            if idx == n:
+                return 0
+            if(dp[idx][buy] != -1):
+                return dp[idx][buy]
+            if buy:
+                profit = max(-prices[idx] + recursion(idx+1,0), 0  + recursion(idx+1,1))
+            else:
+                profit = max(prices[idx] + recursion(idx+1,1), 0  + recursion(idx+1,0))
+            
+            dp[idx][buy] = profit
+            return profit
+        
+        return recursion(0,1)
+        
+        '''
+        recursion tle
+        n = len(prices)
+    
+        def recursion(idx,buy):
+            if idx == n:
+                return 0
+
+            if buy:
+                profit = max(-prices[idx] + recursion(idx+1,0), 0  + recursion(idx+1,1))
+            else:
+                profit = max(prices[idx] + recursion(idx+1,1), 0  + recursion(idx+1,0))
+            
+            return profit
+        
+        return recursion(0,1)
+        '''
