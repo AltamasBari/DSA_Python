ab518a5dba5f6f0ca647729abc48b29581ddb9cd
Altamas Bari <altamasbari@gmail.com>
2022-05-12 21:20:01 +0530

    Time: 83 ms (51.47%), Space: 15.3 MB (22.94%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        #use 2 variable dp or 1d array of size 2 tabulation dp
         n = len(prices)
-        dp = [[0 for j in range(2)] for i in range(n+1)]  
+        nextNotBuy = 0
+        nextBuy = 0
 
         for idx in reversed(range(n)):
+            curBuy = max(-prices[idx] + nextNotBuy, nextBuy)
+            curNotBuy = max(prices[idx] + nextBuy, nextNotBuy)
+            
+            nextNotBuy = curNotBuy
+            nextBuy = curBuy
 
+        return nextBuy       
+        
+        '''
+        2d tabulation dp
+        n = len(prices)
+        dp = [[0 for j in range(2)] for i in range(n+1)]  
+        dp[n][0] = dp[n][1] = 0
+        for idx in reversed(range(n)):
             for buy in range(2):
                 if buy:
                     profit = max(-prices[idx] + dp[idx+1][0], dp[idx+1][1])
@@ -14,7 +29,7 @@ class Solution:
                 dp[idx][buy] = profit
 
         return dp[0][1]
-        
+        '''
         '''
         recursion memoization
         n = len(prices)
