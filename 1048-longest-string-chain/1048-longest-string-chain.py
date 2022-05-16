e1e60e67c4b443e946951898787a5f5a831a00b8
Altamas Bari <altamasbari@gmail.com>
2022-05-16 20:18:20 +0530

    Time: 2980 ms (6.91%), Space: 14.3 MB (72.64%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -13,15 +13,16 @@ class Solution:
             
             i = 0
             j = 0
-
+            count = 0
             while i < p:
                 if j < q and x[i] == y[j]:
                     i += 1
                     j += 1
                 else:
                     i += 1
+                    count += 1
             
-            if i == p and j == q:
+            if count == 1:
                 return True
             else:
                 return False
