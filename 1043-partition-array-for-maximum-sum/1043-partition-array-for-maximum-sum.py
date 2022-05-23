f419dcd65fcf4c6ff4c52f0642d3d95eb4145ce0
Altamas Bari <altamasbari@gmail.com>
2022-05-23 12:52:14 +0530

    Time: 636 ms (68.35%), Space: 13.9 MB (64.62%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,5 +1,27 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        #Tabulation
+        n = len(arr)
+        dp = [0 for _ in range(n+1)]
+        
+        for i in reversed(range(n)):
+            length = 0
+            maxm = float('-inf')
+            ans = float('-inf')
+            
+            for j in range(i,min(i+k,n)):
+                length += 1
+                maxm = max(maxm,arr[j])
+                sm = (length * maxm) + dp[j+1]
+                ans = max(ans,sm)
+            
+            dp[i] = ans
+        
+        return dp[0]
+        
+        
+        '''
+        #memoization 
         n = len(arr)
         dp = [-1 for _ in range(n)]
         
@@ -23,3 +45,4 @@ class Solution:
             return ans
         
         return recursion(0)
+        '''
