b27ee672a60c61c41beb83a5d0d9355b49e2988f
Altamas Bari <altamasbari@gmail.com>
2022-05-27 19:15:10 +0530

    Time: 202 ms (17.28%), Space: 16.2 MB (6.83%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,5 +1,29 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        #using two pointers
+        n = len(height)
+        left = 0
+        right = n - 1
+        
+        left_max, right_max = 0, 0
+        ans = 0
+        while (left <= right):
+            if height[left] <= height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    ans += (left_max - height[left])
+                left += 1
+            else: 
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    ans += (right_max - height[right]);
+                right -= 1
+        return ans
+        
+        '''
+        #TC -> O(2N) , SC -> O(N)
         n = len(height)
         maxm = float('-inf')
         temp = [0 for _ in range(n)]
@@ -16,4 +40,5 @@ class Solution:
             ans += (temp[i] - height[i])
             
         return ans
+        '''
         
