221293982c33b33c6febad40e5eb692514fec0df
Altamas Bari <altamasbari@gmail.com>
2022-05-30 21:17:12 +0530

    Time: 1866 ms (8.69%), Space: 22.9 MB (51.66%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -1,5 +1,35 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        #Tabulation
+        
+        m = len(s)
+        n = len(p)
+        
+        dp = [[False for _ in range(n+1)] for _ in range(m+1)]
+        
+        dp[0][0] = True
+        
+        for j in range(1, len(p)+1):
+            if p[j-1] != '*':
+                break
+            dp[0][j] = True
+            
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if(s[i-1] == p[j-1] or p[j-1] == '?'):
+                    dp[i][j] = dp[i-1][j-1]
+
+                elif(p[j-1] == '*'):
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j]
+                
+                else:
+                    dp[i][j] = False
+        
+        return dp[m][n]
+            
+        
+        '''
+        #recursion memoization
         m = len(s)
         n = len(p)
         
@@ -33,3 +63,4 @@ class Solution:
             return False
         
         return recursion(m-1,n-1)
+        '''
