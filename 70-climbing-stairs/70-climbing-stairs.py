df69c37deae752dc2b23f49cc24eb62e50b10b08
Altamas Bari <altamasbari@gmail.com>
2022-05-31 15:07:03 +0530

    Time: 37 ms (64.87%), Space: 14 MB (11.98%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,13 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [-1 for _ in range(n+1)]
-        return self.recursion(n,dp)
-    def recursion(self,n,dp):
-        if n == 0:
-            return 1
-        if n == 1:
-            return 1
-        if dp[n] != -1:
+
+        def recursion(n):
+            if n == 0:
+                return 1
+            
+            if n < 0:
+                return 0
+            
+            if dp[n] != -1:
+                return dp[n]
+            
+            dp[n] = recursion(n-1) + recursion(n-2)
             return dp[n]
-        dp[n] = self.recursion(n-1,dp) + self.recursion(n-2,dp)
-        return dp[n]
+        
+        return recursion(n) 
+    
