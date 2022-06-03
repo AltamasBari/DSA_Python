5d0d55a3df6eb6730ddf167dab49054e91598baa
Altamas Bari <altamasbari@gmail.com>
2022-06-03 19:46:00 +0530

    Time: 125 ms (42.85%), Space: 14.2 MB (11.21%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,15 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        #TC - O(2**target * k) k - avg length of subsequence : to insert in ds
+        #SC - unpredictable - k * x, x - no of subsequence possible
         res = []
         ds = []
         n = len(candidates)
         def recursion(idx,target,ds):
             
-            if idx == n:
-                return         
             if target == 0:
                 res.append(ds.copy())
                 return
+            
+            if idx == n:
+                return 
+            
             dont_take = recursion(idx+1,target,ds)
             if target >= candidates[idx]:
                 ds.append(candidates[idx])
