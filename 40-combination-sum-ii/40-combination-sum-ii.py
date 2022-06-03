57a454e8927539fe52e0df0b5ad82c69ae07a08b
Altamas Bari <altamasbari@gmail.com>
2022-06-03 19:46:27 +0530

    Time: 78 ms (75.46%), Space: 14 MB (57.59%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,5 +1,13 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        '''
+        If we use set
+        #TC - O(2**target * k log n) k - avg length of subsequence : to insert in ds,
+        log n: to chech duplicates in set
+        n: size of set
+        #SC - unpredictable - k * x, x - no of subsequence possible
+        '''
+        
         ans = []
         n = len(candidates)
         candidates.sort()
@@ -8,18 +16,18 @@ class Solution:
             if target == 0:
                 ans.append(ds.copy())
                 return
-            
+
             if idx == n:
                 return
             
-            if candidates[idx] > target:
-                return
             
             for i in range(idx,n):
+                
+                if candidates[i] > target:
+                    break
+                
                 if i > idx and candidates[i] == candidates[i-1]:
                     continue
-                    
-
                 ds.append(candidates[i])
                 recursion(i+1,ds,target - candidates[i])
                 ds.remove(candidates[i])
