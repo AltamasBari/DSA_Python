d235ce19c474855a3984fb6bf3b5cafc9bbfcd3d
Altamas Bari <altamasbari@gmail.com>
2022-06-03 21:53:50 +0530

    Time: 464 ms (38.08%), Space: 16.3 MB (80.56%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,8 +1,8 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count  = 0
-        n = len(grid)
-        m = len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
         
         adj = [[-1,0],[1,0],[0,-1],[0,1]]
         
@@ -15,13 +15,13 @@ class Solution:
                 for x,y in adj:
                     row = x+a
                     col = y+b
-                    if row >=0 and row < n and col >=0 and col < m and grid[row][col] == '1':
+                    if row >=0 and row < m and col >=0 and col < n and grid[row][col] == '1':
                         grid[row][col] = '0'
                         q.append([row,col])
             return    
         
-        for i in range(n):
-            for j in range(m):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == '1':
                     bfs(i,j)
                     count += 1
