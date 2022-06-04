c422ab62d28592bbf41c254b9f0d1445d2701fa7
Altamas Bari <altamasbari@gmail.com>
2022-06-04 22:37:39 +0530

    Time: 157 ms (16.63%), Space: 13.9 MB (79.11%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.py
+++ 766-toeplitz-matrix/766-toeplitz-matrix.py
@@ -1,5 +1,16 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        for r in range(m-1):
+            for c in range(n-1):
+                if r+1 < m and c+1 < n and matrix[r][c] != matrix[r+1][c+1] :
+                    return False
+
+        return True
+        '''
+        TC: O(m*n)
+        SC: O(n+2) : n col(n diag) + 2 corner
         d = {}
         m = len(matrix)
         n = len(matrix[0])
@@ -12,3 +23,4 @@ class Solution:
                 d[r-c] = matrix[r][c]
         
         return True
+        '''
