facc85a988031596db6b379c377898628dfbb1ec
Altamas Bari <altamasbari@gmail.com>
2022-06-04 13:55:25 +0530

    Time: 106 ms (46.98%), Space: 14.4 MB (84.03%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,9 +1,9 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:    
     # check whether nth queen can be placed in that column
-        def valid(nums, n):
-            for i in range(n):
-                if abs(nums[i]-nums[n]) == n -i or nums[i] == nums[n]:
+        def valid(nums, idx):
+            for i in range(idx):
+                if abs(nums[i]-nums[idx]) == idx-i or nums[i] == nums[idx]:
                     return False
             return True
     
@@ -11,14 +11,16 @@ class Solution:
     # nums is a one-dimension array, like [1, 3, 0, 2] means
     # first queen is placed in column 1, second queen is placed
     # in column 3, etc.
+    
+    #index = row number
         def dfs(nums, index, path, res):
-            if index == len(nums):
+            if index == n:
                 res.append(path)
                 return  # backtracking
-            for i in range(len(nums)):
+            for i in range(n): # i : col number
                 nums[index] = i
                 if valid(nums, index):  # pruning
-                    tmp = "."*len(nums)
+                    tmp = "." * n # temp = "...."   
                     dfs(nums, index+1, path+[tmp[:i]+"Q"+tmp[i+1:]], res)
         
         res = []
