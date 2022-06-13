67784446040854ed5c28d1edd3a54d56fa531c9e
Altamas Bari <altamasbari@gmail.com>
2022-06-13 11:13:30 +0530

    Time: 105 ms (39.05%), Space: 15.3 MB (20.24%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -2,11 +2,16 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
         dp = [[-1 for j in range(len(triangle[i]))] for i in range(n)]
-        return self.recursion(0,0,n,dp,triangle)
-    def recursion(self,i,j,n,dp,triangle):
-        if i == n-1:
-            return triangle[i][j]
-        if dp[i][j] != -1: return dp[i][j]
-        ans = triangle[i][j] + min(self.recursion(i+1,j,n,dp,triangle),self.recursion(i+1,j+1,n,dp,triangle)) 
-        dp[i][j] = ans
-        return dp[i][j]
+        
+        def recursion(i,j):
+            if i == n:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
+            dp[i][j] = triangle[i][j] + min(recursion(i+1,j),recursion(i+1,j+1)) 
+             
+            return dp[i][j]
+        
+        return recursion(0,0)
