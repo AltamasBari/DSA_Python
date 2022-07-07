4377ef5082dd0b7324f877ba0f802224a227abe7
Altamas Bari <altamasbari@gmail.com>
2022-07-07 20:01:44 +0530

    Time: 262 ms (43.87%), Space: 15.6 MB (34.67%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,5 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        '''
         dict = {}
         for x in nums:
             dict[x] = dict.get(x,0) + 1
@@ -7,3 +8,17 @@ class Solution:
         for x,v in dict.items():    
             if(v > len(nums)//2):
                 return x
+        '''
+        count = 0
+        elem = None
+        
+        for num in nums:
+            if count == 0:
+                elem =  num
+            
+            if elem == num:
+                count += 1
+            else:
+                count -= 1
+        
+        return elem
