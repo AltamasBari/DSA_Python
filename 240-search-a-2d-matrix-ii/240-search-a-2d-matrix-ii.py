d4c8a30357ebed3de979f72f839566ecfc3a49ee
Altamas Bari <altamasbari@gmail.com>
2022-07-24 20:07:00 +0530

    Time: 302 ms (31.39%), Space: 20.3 MB (98.38%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,5 +1,7 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        '''
+        #O(rows * log (columns))
         for row in matrix:
             low = 0
             high = len(row) - 1
@@ -12,4 +14,17 @@ class Solution:
                         low = mid + 1
                     elif row[mid] == target:
                         return True
+        return False
+        '''
+        n = len(matrix)
+        m = len(matrix[0])
+        i = 0
+        j = m - 1
+        while(i >= 0 and i < n and j >= 0 and j < m):
+            if matrix[i][j] > target:
+                j -= 1
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                return True
         return False
