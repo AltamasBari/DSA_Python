071b9cd8a6722435c14cd1b8f4baad6218e0728d
Altamas Bari <altamasbari@gmail.com>
2022-07-27 11:09:47 +0530

    Time: 177 ms (81.16%), Space: 15.4 MB (10.58%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,17 +1,15 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         n = len(nums)
-        temp = [0 for _ in range(n)]
-        sm = 0
-        for i in reversed(range(n)):
-            sm += nums[i]
-            temp[i] = sm
+        
+        cumm_sum = 0
+        for i in range(n):
+            cumm_sum += nums[i]
         
         sm = 0
         for i in range(n):
-            if i != n-1 and sm == temp[i+1]:
-                return i
-            if i == n-1 and sm == 0:
+            cumm_sum -= nums[i]
+            if sm == cumm_sum:
                 return i
             sm += nums[i]
         return -1
