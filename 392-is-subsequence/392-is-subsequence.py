b391b62fd1b28f0541d4de8b1e51bc33e39fb2f3
Altamas Bari <altamasbari@gmail.com>
2022-07-27 17:19:32 +0530

    Time: 79 ms (7.88%), Space: 14 MB (14.15%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -14,10 +14,10 @@ class Solution:
         
         while i < n and j < m:
             if s[i] == t[j]:
-                i += 1
-            
-            if i == n:
-                return True
+                i += 1            
             j += 1
-            
-        return False
+        
+        if i == n:
+            return True
+        else:
+            return False
