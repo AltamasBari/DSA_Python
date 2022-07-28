f0d1ea446af7ed9082f314d510f3542e36ea1738
Altamas Bari <altamasbari@gmail.com>
2022-07-28 11:06:18 +0530

    Time: 94 ms (26.44%), Space: 14.5 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,7 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if(sorted(s) == sorted(t)):
-            return True
-        else:
-            return False
+        arr = [0 for _ in range(26)]
         
+        for x in s:
+            arr[ord(x)-ord('a')] += 1
+        
+        for x in t:
+            if arr[ord(x)-ord('a')]:
+                arr[ord(x)-ord('a')] -= 1
+            else:
+                return False
+        
+        for x in arr:
+            if x != 0:
+                return False
+        return True
