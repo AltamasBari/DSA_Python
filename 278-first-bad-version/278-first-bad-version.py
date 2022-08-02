480ae9dfc3659c6df8ac78ea10b4481946215dac
Altamas Bari <altamasbari@gmail.com>
2022-08-02 22:19:32 +0530

    Time: 36 ms (81.50%), Space: 14 MB (13.05%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -17,14 +17,10 @@ class Solution:
             
             mid = l + (h-l) // 2
             
-            if(isBadVersion(mid)):
-                if(mid != 1 and isBadVersion(mid-1)):
-                    h = mid
-                else:
-                    return mid
-            
+            if(isBadVersion(mid)):               
+                h = mid - 1
             else:
-                l = mid+1
+                l = mid + 1
         
-        return -1
+        return l
         
