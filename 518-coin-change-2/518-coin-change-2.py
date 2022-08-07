7fb1a6324ecf0f6708169a81c39aa63a90792e63
Altamas Bari <altamasbari@gmail.com>
2022-08-07 19:25:59 +0530

    Time: 550 ms (47.71%), Space: 39.2 MB (22.41%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        dp= [[-1 for _ in range(amount+1)] for _ in range(n+1)]
+        
+        def recursion(idx,amt):
+            
+            if amt == 0:
+                dp[idx][amt] =  1
+                return 1
+            
+            if idx == n:
+                return 0
+            
+            if dp[idx][amt] != -1:
+                return dp[idx][amt]
+            
+            not_pick = recursion(idx+1,amt)
+            pick = 0
+            if amt >= coins[idx]:
+                pick = recursion(idx,amt - coins[idx])
+            
+            dp[idx][amt] = pick + not_pick
+            return pick + not_pick
+            
+            
+        return recursion(0,amount)
