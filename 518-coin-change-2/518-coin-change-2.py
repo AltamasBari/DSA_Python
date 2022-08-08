c64b4cc467c31e6902ba3e3ca1ed2b6aae33a2e5
Altamas Bari <altamasbari@gmail.com>
2022-08-08 21:51:08 +0530

    Time: 867 ms (31.49%), Space: 35.1 MB (27.91%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,6 +1,30 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
+        '''
+        dp = [[0 for _ in range(amount+1)] for _ in range(n)]
+        
+        for idx in range(n):
+            dp[idx][0] = 1
+        
+        for amt in range(1,amount+1):
+            if amt % coins[0] == 0:
+                dp[0][amt] = 1
+            else:
+                dp[0][amt] = 0
+        
+        for idx in range(1,n):
+            for amt in range(1,amount+1):
+                not_pick = dp[idx-1][amt]
+                pick = 0
+                if amt >= coins[idx]:
+                    pick = dp[idx][amt - coins[idx]]
+
+                dp[idx][amt] = pick + not_pick
+                
+        return dp[n-1][amount]
+        '''
+        
         
         dp = [[0 for _ in range(amount+1)] for _ in range(n)]
         
@@ -23,7 +47,7 @@ class Solution:
                 dp[idx][amt] = pick + not_pick
                 
         return dp[n-1][amount]
-        
+    
         '''
         dp = [[0 for _ in range(amount+1)] for _ in range(n+1)]
         
