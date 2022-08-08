1a01521b3827b5d63fcead7d788179efbfcd4f26
Altamas Bari <altamasbari@gmail.com>
2022-08-08 21:13:56 +0530

    Time: 803 ms (34.19%), Space: 23.9 MB (49.78%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,6 +1,24 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
+        dp = [[0 for _ in range(amount+1)] for _ in range(n+1)]
+        
+        for i in range(n):
+            dp[i][0] = 1
+        
+        for idx in reversed(range(n)):
+            for amt in range(1,amount+1):
+                not_pick = dp[idx+1][amt]
+                pick = 0
+                if amt >= coins[idx]:
+                    pick = dp[idx][amt - coins[idx]]
+
+                dp[idx][amt] = pick + not_pick
+                
+        return dp[0][amount]
+        
+        '''
+        #Recursion + memoization
         dp= [[-1 for _ in range(amount+1)] for _ in range(n+1)]
         
         def recursion(idx,amt):
@@ -24,3 +42,4 @@ class Solution:
             
             
         return recursion(0,amount)
+        '''
