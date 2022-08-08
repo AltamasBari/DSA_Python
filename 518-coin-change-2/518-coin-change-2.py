98854ddd8b6b4691351d2febbe83e2a48af25a3e
Altamas Bari <altamasbari@gmail.com>
2022-08-08 21:58:50 +0530

    Time: 516 ms (49.90%), Space: 14.3 MB (56.53%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,6 +1,28 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
+        
+        dp = [0 for _ in range(amount+1)] 
+        
+        for amt in range(amount+1):
+            if amt % coins[0] == 0:
+                dp[amt] = 1
+            else:
+                dp[amt] = 0
+        
+        for idx in range(1,n):
+            for amt in range(1,amount+1):
+                not_pick = dp[amt]
+                pick = 0
+                if amt >= coins[idx]:
+                    pick = dp[amt - coins[idx]]
+
+                dp[amt] = pick + not_pick
+        
+        print(dp)
+        return dp[amount]
+        
+        
         '''
         dp = [[0 for _ in range(amount+1)] for _ in range(n)]
         
@@ -21,33 +43,10 @@ class Solution:
                     pick = dp[idx][amt - coins[idx]]
 
                 dp[idx][amt] = pick + not_pick
-                
+        
+        print(dp)
         return dp[n-1][amount]
         '''
-        
-        
-        dp = [[0 for _ in range(amount+1)] for _ in range(n)]
-        
-        for idx in range(n):
-            dp[idx][0] = 1
-        
-        for amt in range(1,amount+1):
-            if amt % coins[0] == 0:
-                dp[0][amt] = 1
-            else:
-                dp[0][amt] = 0
-        
-        for idx in range(1,n):
-            for amt in range(1,amount+1):
-                not_pick = dp[idx-1][amt]
-                pick = 0
-                if amt >= coins[idx]:
-                    pick = dp[idx][amt - coins[idx]]
-
-                dp[idx][amt] = pick + not_pick
-                
-        return dp[n-1][amount]
-    
         '''
         dp = [[0 for _ in range(amount+1)] for _ in range(n+1)]
         
