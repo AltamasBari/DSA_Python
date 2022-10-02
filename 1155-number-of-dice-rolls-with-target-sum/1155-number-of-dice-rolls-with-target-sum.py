0e466e19e73c1f1d908f293676cc42e9a5324c25
Altamas Bari <altamasbari@gmail.com>
2022-10-02 12:30:10 +0530

    Time: 1248 ms (27.85%), Space: 20 MB (24.68%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,17 +1,23 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         modulo = 10**9+7
-
-        @lru_cache(None)
-        def dp(dice_left: int, target_left: int) -> int:
+        
+        dp = [[-1 for _ in range(1005)] for _ in range(32)]
+        
+        def recursion(dice_left: int, target_left: int) -> int:
             if dice_left == 0:
                 if target_left == 0:
                     return 1
                 return 0
+            
+            if dp[dice_left][target_left] != -1:
+                return dp[dice_left][target_left]
+            
             res = 0
             for i in range(1, k+1):
-                res += dp(dice_left-1, target_left-i)
-                res %= modulo
-            return res % modulo
+                res = (res + recursion(dice_left-1, target_left-i)) % modulo
+            
+            dp[dice_left][target_left] = res
+            return dp[dice_left][target_left]
 
-        return dp(n, target)
+        return recursion(n, target)
